4f89cfb2fa52a80aa91284c4caccc39d8b31aa89
Valerii Vladimirov <valeriivladimirov@MacBook-Pro-Valerii.local>
2023-10-12 17:44:48 +0200

    lab1_2

---
+++ pyszne_web/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=64)
+    content = models.TextField()
+    published = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    author = models.CharField(max_length=64)
+    category = models.CharField(max_length=64)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
