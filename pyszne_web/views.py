1a2d1d871611fd84bfd775af4b6a39120237b829
Valerii Vladimirov <valeriivladimirov@MacBook-Pro-Valerii.local>
2023-11-09 15:28:05 +0100

    lab2

--- pyszne_web/views.py
+++ pyszne_web/views.py
@@ -6,7 +6,7 @@ def articles(request):
     all_articles = Article.objects.all()
     return render(request, 'articles.html', {'articles': all_articles})
 
-# articles.html
+
 
 from django.http import HttpResponse
 
